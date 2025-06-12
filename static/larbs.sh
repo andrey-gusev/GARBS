0c14e42c39c4134a2cc2d6ceaa43865cab6163ec
Andrey Gusev <91523312+andrey-gusev@users.noreply.github.com>
2025-06-13 00:24:14 +0500

    Update larbs.sh

--- static/larbs.sh
+++ static/larbs.sh
@@ -6,8 +6,8 @@
 
 ### OPTIONS AND VARIABLES ###
 
-dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/static/progs.csv"
+dotfilesrepo="https://github.com/andrey-gusev/voidrice.git"
+progsfile="https://raw.githubusercontent.com/andrey-gusev/GARBS/refs/heads/master/static/progs.csv"
 aurhelper="yay"
 repobranch="master"
 export TERM=ansi
